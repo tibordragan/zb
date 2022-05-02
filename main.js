4c6b20881dcb442e5c5984784b7e82a73bb0c318
tibordragan <59528105+tibordragan@users.noreply.github.com>
2022-05-02 18:31:05 +0200

    upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// Select element function
+
+const selectElement = function (element) {
+  return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+let navlink = selectElement('.nav-list');
+
+menuToggler.addEventListener('click', function() {
+  body.classList.toggle('open');
+
+});
+
+navlink.addEventListener('click', function() {
+  body.classList.toggle('open');
+
+});
+
+
+
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left', 
+  duration: 1000,
+  distance: '5rem',
+  delay: 200
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right', 
+  duration: 1000,
+  distance: '5rem',
+  delay: 200
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top', 
+  duration: 1000,
+  distance: '5rem',
+  delay: 300
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom', 
+  duration: 1000,
+  distance: '5rem',
+  delay: 300
+});
